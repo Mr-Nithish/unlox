f89ace40c6d3be96ed4ca1085ae8afaac7b7e5b9
Mr-Nithish <officialnithish13@gmail.com>
2025-02-01 15:02:57 +0530

    keyword added

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,27 @@ const robotoThink = localFont({
 });
 
 export const metadata: Metadata = {
-  title: 'Education organisation',
-  description: 'Learn Easly',
+  title: 'Education Organization',
+  description:
+    'Join our online courses for career growth, industry-specific training, and technology upskilling programs. Flexible and interactive digital learning for students and professionals.',
+  keywords: [
+    'edutech company',
+    'online courses',
+    'domain-specific courses',
+    'digital learning for professionals',
+    'online education for students',
+    'skill enhancement courses',
+    'online learning platform',
+    'industry-specific training',
+    'flexible learning',
+    'virtual learning platform',
+    'career growth',
+    'job-oriented courses',
+    'technology training',
+    'upskilling programs',
+    'blended learning solutions',
+    'interactive online courses',
+  ],
 };
 
 export default function RootLayout({
@@ -34,8 +53,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <head>
+        <meta
+          name='keywords'
+          content='edutech company, online courses, domain-specific courses, digital learning for professionals, online education for students, skill enhancement courses, online learning platform, industry-specific training, flexible learning, virtual learning platform, career growth, job-oriented courses, technology training, upskilling programs, blended learning solutions, interactive online courses'
+        />
+      </head>
       <body
         className={`${robotoStrong.variable} ${robotoNote.variable} ${robotoMed.variable} ${robotoThink.variable} antialiased`}
+        aria-label='Education organization providing online learning and upskilling courses'
       >
         {children}
       </body>
